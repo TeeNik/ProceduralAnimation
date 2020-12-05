ec863389b4cb90c2186f25700b9ed2846f9aace4
TeeNik <teenik3315@gmail.com>
2020-12-06 01:27:03 +0300

    leg stepper

---
+++ Assets/Scripts/LegStepper.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegStepper : MonoBehaviour
+{
+
+    [SerializeField] private Transform homeTransform;
+    [SerializeField] private float wantStepAtDistance;
+    [SerializeField] private float moveDuration;
+    [SerializeField] float stepOvershootFraction;
+
+    public bool Moving { get; private set; }
+
+    void Update()
+    {
+        if (!Moving)
+        {
+            float distFromHome = Vector3.Distance(transform.position, homeTransform.position);
+            if (distFromHome > wantStepAtDistance)
+            {
+                StartCoroutine(MoveToHome());
+            }
+        }
+    }
+
+    IEnumerator MoveToHome()
+    {
+        Quaternion startRot = transform.rotation;
+        Vector3 startPoint = transform.position;
+
+        Quaternion endRot = homeTransform.rotation;
+
+        Vector3 towardHome = homeTransform.position - transform.position;
+        float overshootDistance = wantStepAtDistance * stepOvershootFraction;
+        Vector3 overshootVector = towardHome * overshootDistance;
+        overshootVector = Vector3.ProjectOnPlane(overshootVector, Vector3.up);
+
+        Vector3 endPoint = homeTransform.position + overshootVector;
+
+        Vector3 centerPoint = (startPoint + endPoint) / 2;
+        centerPoint += homeTransform.up * Vector3.Distance(startPoint, endPoint) / 2;
+
+        float timeElapsed = 0;
+
+        do
+        {
+            timeElapsed += Time.deltaTime;
+            float normalizedTime = Easing.EaseInOutCubic(timeElapsed / moveDuration);
+
+            transform.position = Vector3.Lerp(
+                Vector3.Lerp(startPoint, centerPoint, normalizedTime),
+                Vector3.Lerp(centerPoint, endPoint, normalizedTime),
+                normalizedTime);
+
+            transform.rotation = Quaternion.Lerp(startRot, endRot, normalizedTime);
+
+            yield return null;
+        }
+        while (timeElapsed < moveDuration);
+
+        Moving = false;
+    }
+}
