826ad2578505caa02c1ded231085f5dad2cec188
TeeNik <teenik3315@gmail.com>
2021-10-08 03:36:39 +0300

    worm test

---
+++ Assets/Worm/Wormy.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class Wormy : MonoBehaviour
+{
+    public Rig rig = null;
+    public Transform target = null;
+    public float Movespeed = 3.5f;
+
+    private void Update()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
+
+        target.Translate(Vector3.up * Movespeed * vert * Time.deltaTime);
+        target.Translate(Vector3.right * Movespeed * horz * Time.deltaTime);
+    }
+}
