e90290743db4b5a300b6e4f6b5e1edb095273d04
TeeNik <teenik3315@gmail.com>
2021-10-24 03:56:34 +0300

    movement and rotation

--- Assets/IK_Test/SpiderController.cs
+++ Assets/IK_Test/SpiderController.cs
@@ -10,9 +10,34 @@ public class SpiderController : MonoBehaviour
     [SerializeField] LegStepper backLeftLegStepper;
     [SerializeField] LegStepper backRightLegStepper;
 
+    [Header("Movement")]
+    public Transform Body;
+    public float MovementSpeed;
+    public float RotationSpeed;
+
     void Awake()
     {
         StartCoroutine(LegUpdateCoroutine());
+    }
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            Body.position = Vector3.MoveTowards(Body.position, Body.position + Body.forward * 10, MovementSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Body.position = Vector3.MoveTowards(Body.position, Body.position - Body.forward * 10, MovementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Body.Rotate(Vector3.up * -RotationSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Body.Rotate(Vector3.up * RotationSpeed * Time.deltaTime);
+        }
     }
 
     IEnumerator LegUpdateCoroutine()
