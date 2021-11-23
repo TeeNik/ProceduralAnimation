d7b5533cf94314afb0a5565b0db81a81cf0231a2
TeeNik <teenik3315@gmail.com>
2021-11-23 06:11:36 +0300

    bezier curve

--- Assets/Scripts/SplineCreator/BezierCurve.cs
+++ Assets/Scripts/SplineCreator/BezierCurve.cs
@@ -9,22 +9,54 @@ public class BezierCurve : MonoBehaviour
 	public void Reset()
 	{
 		points = new Vector3[] {
-			new Vector3(1f, 0f, 0f),
-			new Vector3(2f, 0f, 0f),
-			new Vector3(3f, 0f, 0f)
+			new Vector3(1.0f, 0.0f, 0.0f),
+			new Vector3(2.0f, 0.0f, 0.0f),
+			new Vector3(3.0f, 0.0f, 0.0f),
+			new Vector3(4.0f, 0.0f, 0.0f),
 		};
 	}
 
 	public Vector3 GetPoint(float t)
     {
-		return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], t));
+		return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
+    }
+
+	public Vector3 GetVelocity(float t)
+    {
+		return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) -
+			transform.position;
+    }
+
+	public Vector3 GetDirection(float t)
+    {
+		return GetVelocity(t).normalized;
     }
 }
 
 public static class Bezier
 {
-	public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+	//public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    //{
+	//	return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+    //}
+
+	public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
+	{
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1.0f - t;
+		return oneMinusT * oneMinusT * oneMinusT * p0 +
+			3.0f * oneMinusT * oneMinusT * t * p1 +
+			3.0f * oneMinusT * t * t * p2 +
+			t * t * t * p3;
+	}
+
+	public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t)
     {
-		return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
-    }
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			3.0f * oneMinusT * oneMinusT * (p1 - p0) +
+			6.0f * oneMinusT * t * (p2 - p1) +
+			3.0f * t * t * (p3 - p2);
+	}
 }
--- Assets/Scripts/SplineCreator/BezierCurveInspector.cs
+++ Assets/Scripts/SplineCreator/BezierCurveInspector.cs
@@ -11,6 +11,7 @@ public class BezierCurveInspector : Editor
     private Quaternion rotation;
 
 	private const int lineSteps = 10;
+	private const float directionalScale = 0.5f;
 
     private void OnSceneGUI()
     {
@@ -21,19 +22,14 @@ public class BezierCurveInspector : Editor
 		Vector3 p0 = ShowPoint(0);
 		Vector3 p1 = ShowPoint(1);
 		Vector3 p2 = ShowPoint(2);
+		Vector3 p3 = ShowPoint(3);
 
 		Handles.color = Color.gray;
 		Handles.DrawLine(p0, p1);
-		Handles.DrawLine(p1, p2);
+		Handles.DrawLine(p2, p3);
 
-		Handles.color = Color.white;
-		Vector3 lineStart = curve.GetPoint(0.0f);
-		for(int i = 1; i <= lineSteps; ++i)
-        {
-			Vector3 lineEnd = curve.GetPoint(i / (float)lineSteps);
-			Handles.DrawLine(lineStart, lineEnd);
-			lineStart = lineEnd;
-        }
+		ShowDirections();
+		Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2.0f);
 	}
 
 	private Vector3 ShowPoint(int index)
@@ -49,4 +45,16 @@ public class BezierCurveInspector : Editor
 		}
 		return point;
 	}
+
+	private void ShowDirections()
+    {
+		Handles.color = Color.green;
+		Vector3 point = curve.GetPoint(0.0f);
+		Handles.DrawLine(point, point + curve.GetDirection(0.0f) * directionalScale);
+		for(int i = 1; i <= lineSteps; ++i)
+        {
+			point = curve.GetPoint(i / (float)lineSteps);
+			Handles.DrawLine(point, point + curve.GetDirection(i / (float)lineSteps) * directionalScale);
+        }
+    }
 }
