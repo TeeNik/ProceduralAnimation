acc3ed1aaea241659b698f6062ba8dafc52eda92
TeeNik <teenik3315@gmail.com>
2021-12-22 05:09:11 +0300

    save rotation of root object

--- Assets/Scripts/IK/FabricIK.cs
+++ Assets/Scripts/IK/FabricIK.cs
@@ -16,7 +16,7 @@ public class FabricIK : IKInterface
     public int Iterations = 10;
     public float Delta = 0.001f;
     [Range(0, 1)]
-    public float SnapBackStrength = 1.0f;
+    public float SnapBackStrength = 0.0f;
 
     protected float[] BonesLength;
     protected float CompleteLength;
@@ -27,6 +27,9 @@ public class FabricIK : IKInterface
     protected Quaternion[] StartRotationBone;
     protected Quaternion StartRotationTarget;
     protected Quaternion StartRotationRoot;
+    
+    protected Transform Root;
+
 
     void Start()
     {
@@ -41,10 +44,10 @@ public class FabricIK : IKInterface
 
         StartDirectionSucc = new Vector3[ChainLength + 1];
         StartRotationBone = new Quaternion[ChainLength + 1];
-        StartRotationTarget = Target.rotation;
+        StartRotationTarget = GetRotationRootSpace(Target);
 
         CompleteLength = 0.0f;
-
+        Root = transform;
         var current = transform.GetChild(0);
         for (int i = 0; i < Bones.Length; ++i)
         {
@@ -58,16 +61,16 @@ public class FabricIK : IKInterface
         for (int i = Bones.Length - 1; i >= 0; --i)
         {
             current = Bones[i];
-            StartRotationBone[i] = current.rotation;
+            StartRotationBone[i] = GetRotationRootSpace(current);
 
             if(i == Bones.Length - 1)
             {
-                StartDirectionSucc[i] = Target.position - current.position;
+                StartDirectionSucc[i] = GetPositionRootSpace(Target) - GetPositionRootSpace(current);
             }
             else
             {
-                StartDirectionSucc[i] = Bones[i + 1].position - current.position;
-                BonesLength[i] = (Bones[i + 1].position - current.position).magnitude;
+                StartDirectionSucc[i] = GetPositionRootSpace(Bones[i + 1]) - GetPositionRootSpace(current);
+                BonesLength[i] = StartDirectionSucc[i].magnitude;
                 CompleteLength += BonesLength[i];
             }
         }
@@ -92,15 +95,18 @@ public class FabricIK : IKInterface
 
         for(int i = 0; i < Bones.Length; ++i)
         {
-            Positions[i] = Bones[i].position;
+            Positions[i] = GetPositionRootSpace(Bones[i]);
         }
+
+        var targetPosition = GetPositionRootSpace(Target);
+        var targetRotation = GetRotationRootSpace(Target);
 
         var rootRot = (Bones[0].parent != null) ? Bones[0].parent.rotation : Quaternion.identity;
         var rootRotDiff = rootRot * Quaternion.Inverse(StartRotationRoot);
 
-        if((Target.position - Bones[0].position).sqrMagnitude >= CompleteLength * CompleteLength)
+        if((targetPosition - GetPositionRootSpace(Bones[0])).sqrMagnitude >= CompleteLength * CompleteLength)
         {
-            var direction = (Target.position - Positions[0]).normalized;
+            var direction = (targetPosition - Positions[0]).normalized;
             for (int i = 1; i < Positions.Length; ++i)
             {
                 Positions[i] = Positions[i - 1] + direction * BonesLength[i - 1];
@@ -117,7 +123,7 @@ public class FabricIK : IKInterface
                 {
                     if (i == Positions.Length - 1)
                     {
-                        Positions[i] = Target.position;
+                        Positions[i] = targetPosition;
                     }
                     else
                     {
@@ -130,7 +136,7 @@ public class FabricIK : IKInterface
                     Positions[i] = Positions[i - 1] + (Positions[i] - Positions[i - 1]).normalized * BonesLength[i - 1];
                 }
 
-                if((Positions[Positions.Length - 1] - Target.position).sqrMagnitude < Delta * Delta)
+                if((Positions[Positions.Length - 1] - targetPosition).sqrMagnitude < Delta * Delta)
                 {
                     break;
                 }
@@ -139,10 +145,11 @@ public class FabricIK : IKInterface
 
         if (Pole != null)
         {
-            for(int i = 1; i < Positions.Length - 1; ++i)
+            var polePosition = GetPositionRootSpace(Pole);
+            for (int i = 1; i < Positions.Length - 1; ++i)
             {
                 var plane = new Plane(Positions[i + 1] - Positions[i - 1], Positions[i - 1]);
-                var projectedPole = plane.ClosestPointOnPlane(Pole.position);
+                var projectedPole = plane.ClosestPointOnPlane(polePosition);
                 var projectedBone = plane.ClosestPointOnPlane(Positions[i]);
                 var angle = Vector3.SignedAngle(projectedBone - Positions[i - 1], projectedPole - Positions[i - 1], plane.normal);
                 Positions[i] = Quaternion.AngleAxis(angle, plane.normal) * (Positions[i] - Positions[i - 1]) + Positions[i - 1];
@@ -152,10 +159,10 @@ public class FabricIK : IKInterface
         for (int i = 0; i < Positions.Length; ++i)
         {
             if (i == Positions.Length - 1)
-                Bones[i].rotation = Target.rotation * Quaternion.Inverse(StartRotationTarget) * StartRotationBone[i];
+                SetRotationRootSpace(Bones[i], Quaternion.Inverse(targetRotation) * StartRotationTarget * Quaternion.Inverse(StartRotationBone[i]));
             else
-                Bones[i].rotation = Quaternion.FromToRotation(StartDirectionSucc[i], Positions[i + 1] - Positions[i]) * StartRotationBone[i];
-            Bones[i].position = Positions[i];
+                SetRotationRootSpace(Bones[i], Quaternion.FromToRotation(StartDirectionSucc[i], Positions[i + 1] - Positions[i]) * Quaternion.Inverse(StartRotationBone[i]));
+            SetPositionRootSpace(Bones[i], Positions[i]);
         }
 
         List<Vector3> localPositions = new List<Vector3>();
@@ -164,6 +171,39 @@ public class FabricIK : IKInterface
             localPositions.Add(Bones[0].transform.InverseTransformPoint(bone.position));
         }
         OnBonesUpdated?.Invoke(localPositions.ToArray());
+    }
+
+    private Vector3 GetPositionRootSpace(Transform current)
+    {
+        if (Root == null)
+            return current.position;
+        else
+            return Quaternion.Inverse(Root.rotation) * (current.position - Root.position);
+    }
+
+    private void SetPositionRootSpace(Transform current, Vector3 position)
+    {
+        if (Root == null)
+            current.position = position;
+        else
+            current.position = Root.rotation * position + Root.position;
+    }
+
+    private Quaternion GetRotationRootSpace(Transform current)
+    {
+        //inverse(after) * before => rot: before -> after
+        if (Root == null)
+            return current.rotation;
+        else
+            return Quaternion.Inverse(current.rotation) * Root.rotation;
+    }
+
+    private void SetRotationRootSpace(Transform current, Quaternion rotation)
+    {
+        if (Root == null)
+            current.rotation = rotation;
+        else
+            current.rotation = Root.rotation * rotation;
     }
 
     private void OnDrawGizmos()
