bd298bffab59212d8a545b0e132303ba4fe88ebe
TeeNik <teenik3315@gmail.com>
2021-12-26 05:31:56 +0300

    leg orientation

--- Assets/Scripts/IK/LegOrientation.cs
+++ Assets/Scripts/IK/LegOrientation.cs
@@ -7,13 +7,34 @@ public class LegOrientation : MonoBehaviour
     [SerializeField] Transform PointA;
     [SerializeField] Transform PointB;
 
+    public bool SetForward = true;
+
+    private Vector3 Right;
+    private Vector3 Forward;
+
+    private void Awake()
+    {
+        Right = transform.right;
+        Forward = transform.forward;
+    }
+
     public void UpdateOrientation()
     {
         if(PointA && PointB)
         {
             Vector3 middle = (PointB.position + PointA.position) / 2;
             transform.position = middle;
-            transform.forward = (PointB.position - PointA.position).normalized;
+
+            if(SetForward)
+            {
+                transform.forward = (PointB.position - PointA.position).normalized;
+                //transform.right = Right;
+            }
+            else
+            {
+                transform.right = (PointB.position - PointA.position).normalized;
+                //transform.forward = Forward;
+            }
         }
     }
 }
