218c60d85d4dbb8c5890078d0ec353b4ef509055
TeeNik <teenik3315@gmail.com>
2022-01-03 00:19:16 +0300

    update height on awake

--- Assets/Scripts/Centipede/CentipedeBodyPartNew.cs
+++ Assets/Scripts/Centipede/CentipedeBodyPartNew.cs
@@ -26,6 +26,25 @@ public class CentipedeBodyPartNew : MonoBehaviour
         LeftLegStepper = leftLegStepper;
     }
 
+    private void Awake()
+    {
+        UpdateHeight();
+    }
+
+    void UpdateHeight()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.up * -1, out hit, 100.0f))
+        {
+            var heightDiff = Vector3.Distance(transform.position, hit.point);
+            if (heightDiff > BodyHeightBase)
+            {
+                transform.position = hit.point + hit.normal * BodyHeightBase;
+            }
+        }
+    }
+
+
     private void Update()
     {
         if(!IsHead)
@@ -35,13 +54,27 @@ public class CentipedeBodyPartNew : MonoBehaviour
 
             if(dist > DistanceToLeader)
             {
-                float speed = Mathf.Min(5.0f, dist / DistanceToLeader);
-                transform.position = Vector3.MoveTowards(transform.position, Leader.BackPivot.position, FollowSpeed * Time.deltaTime);
-
-                if (Vector3.Angle(dir, FrontPivot.forward) > 5.0f)
+                //float speed = Mathf.Min(5.0f, dist / DistanceToLeader);
+                float speed = Mathf.Min(FollowSpeed * Time.deltaTime, dist * 0.95f); ;
+                Vector3 move = Vector3.MoveTowards(transform.position, Leader.BackPivot.position, speed);
+                if((move - transform.position).magnitude > dist)
                 {
-                    Quaternion toRotation = Quaternion.LookRotation(dir);
-                    transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, 10 * Time.deltaTime);
+                    Debug.Log("bad");
+                }
+                //move = Vector3.ClampMagnitude(move, DistanceToLeader);
+                transform.position = move;
+
+                if (Vector3.Angle(dir, FrontPivot.forward) > 15.0f)
+                {
+                    float z = transform.rotation.eulerAngles.z;
+
+                    Quaternion toRotation = Quaternion.LookRotation(dir, transform.up);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, 1 * Time.deltaTime);
+                    //float z = transform.rotation.eulerAngles.z;
+                    //Vector3 newDirection = Vector3.RotateTowards(transform.forward, dir.normalized, 1 * Time.deltaTime, 0.0f);
+                    //Quaternion look = Quaternion.LookRotation(newDirection);
+                    //look.eulerAngles = new Vector3(look.eulerAngles.x, look.eulerAngles.y, z);
+                    //transform.rotation = look;
                 }
             }
 
@@ -55,7 +88,7 @@ public class CentipedeBodyPartNew : MonoBehaviour
             if (Physics.Raycast(transform.position, transform.up * -1, out hit, 10.0f))
             {
                 var heightDiff = Vector3.Distance(transform.position, hit.point);
-                if (heightDiff < BodyHeightBase)
+                if (heightDiff > BodyHeightBase)
                 {
                     Vector3 position = transform.position + transform.up * (BodyHeightBase - heightDiff);
                     transform.position = Vector3.Lerp(transform.position, position, 0.05f);
