776572bc30d0ddc22e03092540f864fe56d664c9
TeeNik <teenik3315@gmail.com>
2022-01-19 04:12:49 +0300

    update movement

--- Assets/Scripts/AI/AIMovement.cs
+++ Assets/Scripts/AI/AIMovement.cs
@@ -4,11 +4,17 @@ using System;
 
 public class AIMovement : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent Agent;
+    [SerializeField] protected NavMeshAgent Agent;
+    [SerializeField] protected Transform Body;
 
     [SerializeField] protected Transform Target;
     protected Action OnDestinationReached;
     protected bool IsMoving = false;
+
+    private void Awake()
+    {
+        Agent.updatePosition = false;
+    }
 
     private void Start()
     {
@@ -22,6 +28,10 @@ public class AIMovement : MonoBehaviour
     {
         if (target != null)
         {
+            transform.position = Body.position;
+            transform.rotation = Body.rotation;
+            Agent.enabled = true;
+
             Target = target;
             Agent.SetDestination(Target.position);
             Agent.isStopped = false;
@@ -33,12 +43,18 @@ public class AIMovement : MonoBehaviour
     {
         IsMoving = false;
         Agent.isStopped = true;
+        Agent.enabled = false;
     }
 
     protected virtual void Update()
     {
         if (!Agent.pathPending && IsMoving)
         {
+            Vector3 nextPos = Agent.nextPosition;
+            transform.position = nextPos;
+            Body.position = nextPos;
+            Body.rotation = Quaternion.Euler(new Vector3(Body.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, Body.rotation.eulerAngles.z));
+
             if (Agent.remainingDistance <= Agent.stoppingDistance)
             {
                 if (!Agent.hasPath || Agent.velocity.sqrMagnitude == 0f)
