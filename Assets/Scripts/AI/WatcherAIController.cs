cb7593ce6e1ee92ff4fa9a2331482d365495768d
TeeNik <teenik3315@gmail.com>
2022-01-21 04:13:46 +0300

    draw path

--- Assets/Scripts/AI/WatcherAIController.cs
+++ Assets/Scripts/AI/WatcherAIController.cs
@@ -36,4 +36,29 @@ public class WatcherAIController : AIMovement
             ChooseNextTarget();
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        var nav = Agent;
+        if (nav == null || nav.path == null)
+            return;
+
+        var line = this.GetComponent<LineRenderer>();
+        if (line == null)
+        {
+            line = this.gameObject.AddComponent<LineRenderer>();
+            line.material = new Material(Shader.Find("Sprites/Default")) { color = Color.yellow };
+            line.SetWidth(0.5f, 0.5f);
+            line.SetColors(Color.yellow, Color.yellow);
+        }
+
+        var path = nav.path;
+
+        line.SetVertexCount(path.corners.Length);
+
+        for (int i = 0; i < path.corners.Length; i++)
+        {
+            line.SetPosition(i, path.corners[i]);
+        }
+    }
 }
