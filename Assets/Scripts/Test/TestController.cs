03a4b6c637f42bd02ca264b8b5c6ed92eefec13f
TeeNik <teenik3315@gmail.com>
2022-02-02 16:19:58 +0300

    climbing

--- Assets/Scripts/Test/TestController.cs
+++ Assets/Scripts/Test/TestController.cs
@@ -18,7 +18,8 @@ public class TestController : MonoBehaviour
 
 
     private int RayIndex = 0;
-
+    private Vector3 MoveTarget = Vector3.zero;
+    private Quaternion RotationTarget = Quaternion.identity;
 
     void Awake()
     {
@@ -48,12 +49,18 @@ public class TestController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            Body.position = Vector3.MoveTowards(Body.position, Body.position + Body.forward * 10, MovementSpeed * Time.deltaTime);
+            //Body.position = Vector3.MoveTowards(Body.position, Body.position + Body.forward * 10, MovementSpeed * Time.deltaTime);
+            Body.position = Vector3.MoveTowards(Body.position, MoveTarget, MovementSpeed * Time.deltaTime);
+
+            Vector3 lTargetDir = MoveTarget - Body.position;
+            //transform.rotation = Quaternion.RotateTowards(Body.rotation, Quaternion.LookRotation(lTargetDir), RotationSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.RotateTowards(Body.rotation, RotationTarget, RotationSpeed * Time.deltaTime);
+
         }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            Body.position = Vector3.MoveTowards(Body.position, Body.position - Body.forward * 10, MovementSpeed * Time.deltaTime);
-        }
+        //else if (Input.GetKey(KeyCode.S))
+        //{
+        //    Body.position = Vector3.MoveTowards(Body.position, Body.position - Body.forward * 10, MovementSpeed * Time.deltaTime);
+        //}
         if (Input.GetKey(KeyCode.A))
         {
             Body.Rotate(Vector3.up * -RotationSpeed * Time.deltaTime);
@@ -83,6 +90,10 @@ public class TestController : MonoBehaviour
 
     private Vector3 HitPos =Vector3.zero;
 
+
+    public float RaycastHeight = 1.0f;
+    public float RaycastForwardDist = 0.5f;
+
     private void CheckObstacle()
     {
         //Vector3 up = Body.up;
@@ -94,19 +105,15 @@ public class TestController : MonoBehaviour
         //Debug.DrawLine(Body.position, upPos);
         //Debug.DrawLine(upPos, forPos);
 
-
-
-        const float height = 2.0f;
-        const float forwardDist = 1.0f;
         const float angle = 270.0f;
         const float step = 15.0f;
 
         for(int i = 0; i <= angle / step; ++i)
         {
             Vector3 up = Quaternion.AngleAxis(step * i, Body.right) * Body.up;
-            Vector3 upPos = Body.position + up * height;
+            Vector3 upPos = Body.position + up * RaycastHeight;
             Vector3 forward = Quaternion.AngleAxis(step * i, Body.right) * Body.forward;
-            Vector3 forPos = upPos + forward * forwardDist;
+            Vector3 forPos = upPos + forward * RaycastForwardDist;
 
             Debug.DrawLine(Body.position, upPos);
             Debug.DrawLine(upPos, forPos);
@@ -117,16 +124,34 @@ public class TestController : MonoBehaviour
             if (Physics.Linecast(upPos, forPos, out hit, groundRaycastMask))
             {
                 HitPos = hit.point;
+                MoveTarget = hit.point + hit.normal * BodyHeightBase;
+
+                Vector3 newForward = Vector3.Cross(Body.right, hit.normal);
+                DrawCoordSystem(MoveTarget, newForward, hit.normal, Body.right);
+                RotationTarget = Quaternion.LookRotation(newForward, hit.normal);
+
                 break;
             }
         }
 
     }
 
+    void DrawCoordSystem(Vector3 origin, Vector3 forward, Vector3 up, Vector3 right)
+    {
+        const float length = 1.0f;
+        Debug.DrawLine(origin, origin + forward * length, Color.blue);
+        Debug.DrawLine(origin, origin + up * length, Color.green);
+        Debug.DrawLine(origin, origin + right * length, Color.red);
+    }
+
+
     void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawSphere(HitPos, 0.2f);
+        Gizmos.DrawSphere(HitPos, 0.1f);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(MoveTarget, 0.1f);
     }
 
 }
